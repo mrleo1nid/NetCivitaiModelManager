9bfd54e191706e7996e2e59fdf93c66b637ef127
mrleo1nid <43583385+mrleo1nid@users.noreply.github.com>
2023-03-10 08:15:29 +0300

    add template to downoload

---
+++ NetCivitaiModelManager/Views/DownoloadControlView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace NetCivitaiModelManager.Views
+{
+    public partial class DownoloadControlView : UserControl
+    {
+        public DownoloadControlView()
+        {
+            InitializeComponent();
+        }
+    }
+}
